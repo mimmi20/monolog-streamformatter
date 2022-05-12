4543e3ecd25613b226eccf6aca6a178c6de62a39
Thomas Müller <thomas.mueller@jungdms.de>
2022-05-12 12:47:17 +0200

    update StreamFormatter

--- src/StreamFormatter.php
+++ src/StreamFormatter.php
@@ -5,6 +5,8 @@ namespace Mimmi20\Monolog\Formatter;
 use Monolog\Formatter\NormalizerFormatter;
 use Monolog\Utils;
 use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableCell;
+use Symfony\Component\Console\Helper\TableSeparator;
 use Symfony\Component\Console\Output\BufferedOutput;
 
 /**
@@ -12,17 +14,22 @@ use Symfony\Component\Console\Output\BufferedOutput;
  */
 class StreamFormatter extends NormalizerFormatter
 {
-    private const SIMPLE_FORMAT = "[%datetime%] %channel%.%level_name%: %message%";
+    private const SIMPLE_FORMAT = "%message%";
 
+    private string $format;
+    private string $tableStyle;
     private bool $allowInlineLineBreaks;
     private bool $includeStacktraces;
 
     /**
+     * @param string|null $format                     The format of the message
      * @param string|null $dateFormat                 The format of the timestamp: one supported by DateTime::format
      * @param bool        $allowInlineLineBreaks      Whether to allow inline line breaks in log entries
      */
-    public function __construct(?string $dateFormat = null, bool $allowInlineLineBreaks = false, bool $includeStacktraces = false)
+    public function __construct(?string $format = null, string $tableStyle = 'box', ?string $dateFormat = null, bool $allowInlineLineBreaks = false, bool $includeStacktraces = false)
     {
+        $this->format = $format === null ? self::SIMPLE_FORMAT : $format;
+        $this->tableStyle = $tableStyle;
         $this->allowInlineLineBreaks = $allowInlineLineBreaks;
         $this->includeStacktraces($includeStacktraces);
 
@@ -52,7 +59,31 @@ class StreamFormatter extends NormalizerFormatter
     {
         $vars = parent::format($record);
 
-        $message = self::SIMPLE_FORMAT;
+        $message = $this->format;
+
+        foreach ($vars['extra'] as $var => $val) {
+            if (false !== strpos($message, '%extra.'.$var.'%')) {
+                $message = str_replace('%extra.'.$var.'%', $this->stringify($val), $message);
+                unset($vars['extra'][$var]);
+            }
+        }
+
+        foreach ($vars['context'] as $var => $val) {
+            if (false !== strpos($message, '%context.'.$var.'%')) {
+                $message = str_replace('%context.'.$var.'%', $this->stringify($val), $message);
+                unset($vars['context'][$var]);
+            }
+        }
+
+        if (empty($vars['context'])) {
+            unset($vars['context']);
+            $message = str_replace('%context%', '', $message);
+        }
+
+        if (empty($vars['extra'])) {
+            unset($vars['extra']);
+            $message = str_replace('%extra%', '', $message);
+        }
 
         foreach ($vars as $var => $val) {
             if (false !== strpos($message, '%'.$var.'%')) {
@@ -61,21 +92,68 @@ class StreamFormatter extends NormalizerFormatter
         }
 
         $output = new BufferedOutput();
+        $output->writeln(str_repeat('=', 220));
+        $output->writeln('');
         $output->writeln($message);
         $output->writeln('');
 
         $table = new Table($output);
-        $table->setHeaders(['Title', '']);
+        $table->setStyle($this->tableStyle);
+        $table->setColumnWidths([20, 20, 60]);
+        $table->setHeaderTitle($record['level_name']);
+        $table->setHeaders([new TableCell('General Info', ['colspan' => 3])]);
+
+        $table->addRow(['Time', new TableCell($record['datetime']->format($this->dateFormat), ['colspan' => 2])]);
+        $table->addRow(['Level', new TableCell($record['level_name'], ['colspan' => 2])]);
+
+        $output->writeln('');
 
         foreach (array('extra', 'context') as $element) {
             if (empty($record[$element])) {
                 continue;
             }
 
-            $table->addRow(['Time', $record['datetime']->format($this->dateFormat)]);
-            $table->addRow(['Level', $record['level_name']]);
+            $table->addRow(new TableSeparator());
+            $table->addRow([new TableCell(ucfirst($element), ['colspan' => 3])]);
+            $table->addRow(new TableSeparator());
 
             foreach($record[$element] as $key => $value){
+                if ($value instanceof \Throwable) {
+                    $exception = $value;
+
+                    $value = [
+                        'Type' => get_class($exception),
+                        'Message' => $exception->getMessage(),
+                        'Code' => $exception->getCode(),
+                        'File' => $exception->getFile(),
+                        'Line' => $exception->getLine(),
+                        'Trace' => $exception->getTraceAsString(),
+                    ];
+
+                    $this->addFact($table, $key, $value);
+
+                    $prev = $exception->getPrevious();
+
+                    if ($prev instanceof \Throwable) {
+                        do {
+                            $value = [
+                                'Type' => get_class($prev),
+                                'Message' => $prev->getMessage(),
+                                'Code' => $prev->getCode(),
+                                'File' => $prev->getFile(),
+                                'Line' => $prev->getLine(),
+                                'Trace' => $prev->getTraceAsString(),
+                            ];
+
+                            $this->addFact($table, 'previous Throwable', $value);
+
+                            $prev = $prev->getPrevious();
+                        } while ($prev instanceof \Throwable);
+                    }
+
+                    continue;
+                }
+
                 $this->addFact($table, $key, $value);
             }
         }
@@ -143,12 +221,24 @@ class StreamFormatter extends NormalizerFormatter
     {
         $name = trim(str_replace('_', ' ', $name));
 
+        if (is_array($value)) {
+            $rowspan = count($value);
+
+            foreach (array_keys($value) as $number => $key) {
+                if (0 === $number) {
+                    $table->addRow([new TableCell(ucfirst($name), ['rowspan' => $rowspan]), $key, $value[$key]]);
+                } else {
+                    $table->addRow([$key, $value[$key]]);
+                }
+            }
+
+            return;
+        }
+
         if (!is_string($value)) {
             $value = $this->stringify($value);
         }
 
-        $return = !empty($value) ? substr($value, 0, 1000) : '';
-
-        $table->addRow([ucfirst($name), $return]);
+        $table->addRow([ucfirst($name), new TableCell($value, ['colspan' => 2])]);
     }
 }
