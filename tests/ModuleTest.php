d9308c4bae0e81d62c4d0f4ab1b5796232ca75a4
Thomas Müller <mimmi20@live.de>
2023-05-28 21:00:07 +0200

    add more tests

--- tests/ModuleTest.php
+++ tests/ModuleTest.php
@@ -28,5 +28,26 @@ final class ModuleTest extends TestCase
         self::assertIsArray($config);
         self::assertCount(1, $config);
         self::assertArrayHasKey('monolog_formatters', $config);
+
+        $monologFormatterConfig = $config['monolog_formatters'];
+        self::assertIsArray($monologFormatterConfig);
+        self::assertCount(2, $monologFormatterConfig);
+
+        self::assertArrayNotHasKey('abstract_factories', $monologFormatterConfig);
+        self::assertArrayNotHasKey('delegators', $monologFormatterConfig);
+        self::assertArrayNotHasKey('initializers', $monologFormatterConfig);
+        self::assertArrayNotHasKey('invokables', $monologFormatterConfig);
+        self::assertArrayNotHasKey('services', $monologFormatterConfig);
+        self::assertArrayNotHasKey('shared', $monologFormatterConfig);
+
+        self::assertArrayHasKey('aliases', $monologFormatterConfig);
+        $aliases = $monologFormatterConfig['aliases'];
+        self::assertIsArray($aliases);
+        self::assertCount(1, $aliases);
+
+        self::assertArrayHasKey('factories', $monologFormatterConfig);
+        $factories = $monologFormatterConfig['factories'];
+        self::assertIsArray($factories);
+        self::assertCount(1, $factories);
     }
 }
