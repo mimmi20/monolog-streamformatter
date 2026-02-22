f3b69389359dbb2053c6b4b8d0958621afc7746a
Thomas Müller <mimmi20@live.de>
2026-02-22 10:38:29 +0100

    fix issue

--- src/StreamFormatter.php
+++ src/StreamFormatter.php
@@ -33,7 +33,7 @@ use function is_scalar;
 use function is_string;
 use function str_repeat;
 use function str_replace;
-use function mb_trim;
+use function trim;
 
 final class StreamFormatter extends NormalizerFormatter
 {
@@ -155,7 +155,7 @@ final class StreamFormatter extends NormalizerFormatter
 
         $this->output->writeln(str_repeat('=', self::FULL_WIDTH));
         $this->output->writeln('');
-        $this->output->writeln(mb_trim($message));
+        $this->output->writeln(trim($message));
         $this->output->writeln('');
 
         $this->table->setHeaders([
@@ -302,7 +302,7 @@ final class StreamFormatter extends NormalizerFormatter
     /** @throws RuntimeException if encoding fails and errors are not ignored */
     private function addFact(string $name, mixed $value): void
     {
-        $name = mb_trim(str_replace('_', ' ', $name));
+        $name = trim(str_replace('_', ' ', $name));
 
         if (is_array($value)) {
             $rowspan = count($value);
